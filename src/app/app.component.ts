8b25ff757de5da84aefd2f29d52d9508aadd9666
Jenny Lewis-Gandy <jenny.lewis-gandy@voxpopme.com>
2022-11-03 15:48:03 +0000

    comparing Max's code to mine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,36 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  @ViewChild('f') signUpForm: NgForm;
-
   defaultSubscription = "advanced";
 
+  // Max code start
+  subscriptions = ["Basic", "Advanced", "Pro"];
+  selectedSubscription = this.subscriptions[1];
+  // Max code end
+
+
+
+  @ViewChild('signupForm') suForm: NgForm;
+
+  // Max code start
+  @ViewChild('signupForm', { static: false }) signUpForm: NgForm;
+  // Max code end
+
+
+
+  // Max code start
   onSubmit() {
     console.log("value", this.signUpForm.value);
     console.log("email", this.signUpForm.value.email);
     console.log("subscriptions", this.signUpForm.value.subscriptions)
     console.log("password", this.signUpForm.value.password)
+  }
+  // Max code end
+
+  onSubmitMyForm() {
+    console.log("value", this.suForm.value);
+    console.log("email", this.suForm.value.email);
+    console.log("subscriptions", this.suForm.value.subscriptions)
+    console.log("password", this.suForm.value.password)
   }
 }
